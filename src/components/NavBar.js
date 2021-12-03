94c49a48b58391f3a8849aaffaf3f72b1023359d
xbenliimad <xbenliimad@gmail.com>
2021-12-03 16:39:29 +0100

    Fixing the mobile sidebar

---
+++ src/components/NavBar.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { VscThreeBars } from "react-icons/vsc";
+import { IoMdClose } from "react-icons/io";
+
+export default function NavBar() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenClose = () => {
+    setOpen(!open);
+    console.log("HandleOpenClose is working");
+  };
+
+  return (
+    <>
+      <nav className={open ? "sidebar-container" : "navbar-container"}>
+        {/* lOGO */}
+        <h1 className={open ? "logo sidebar-logo" : "logo sidebar-logo"}>
+          AGENCY
+        </h1>
+
+        <figure
+          className={open ? "close-icon" : "none"}
+          onClick={handleOpenClose}
+        >
+          <IoMdClose color="white" />
+        </figure>
+
+        {/* Navigation menu */}
+        <ul className={open ? "sidebar-menu" : "nav-menu"}>
+          <li>
+            <a href="/">Discover</a>
+          </li>
+          <li>
+            <a href="/">Services</a>
+          </li>
+          <li>
+            <a href="/">About us</a>
+          </li>
+          <li>
+            <a href="/">Contact us</a>
+          </li>
+        </ul>
+        {/* Sign in and sign up buttons */}
+        <ul className={open ? "sign-in-up sidebar-sign-in-up" : "sign-in-up"}>
+          <li>
+            <a href="/">SIGN UP</a>
+          </li>
+          <li id="sign-in">
+            <a href="/">SIGN IN</a>
+          </li>
+        </ul>
+
+        <figure
+          className={open ? "none" : "mobile-icon"}
+          onClick={handleOpenClose}
+        >
+          <VscThreeBars color="white" />
+        </figure>
+      </nav>
+    </>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,10 @@
-import React, { useState } from 'react'
-import NavBar from '../components/navbar/NavBar'
-import SideBarMenu from '../components/navbar/SideBarMenu'
+import React from "react";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
-
-    const [open, setOpen] = useState(false)
-
-    const handleOpenClose = () => {
-        setOpen(!open)
-        console.log('HandleOpenClose is working')
-    }
-
-    return (
-        <>
-            <NavBar handleOpenClose={handleOpenClose} />
-            {open && <SideBarMenu open={open} handleOpenClose={handleOpenClose} />}
-        </>
-    )
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
